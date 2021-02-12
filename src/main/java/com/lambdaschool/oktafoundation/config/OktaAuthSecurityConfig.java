0021eded99c8d426ea264fc338e9f453a20fbb82
bhawnishk1 <mrbhawnish@gmail.com>
2021-02-12 11:54:41 -0800

    Added dependencies + okta setup with JwtAuthfilter

--- src/main/java/com/lambdaschool/oktafoundation/config/OktaAuthSecurityConfig.java
+++ src/main/java/com/lambdaschool/oktafoundation/config/OktaAuthSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 // This allows us to further restrict access to an endpoint inside of a controller.
@@ -24,6 +25,8 @@ public class OktaAuthSecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
         http.authorizeRequests()
             .antMatchers("/",
                 "/h2-console/**",
@@ -33,15 +36,6 @@ public class OktaAuthSecurityConfig extends WebSecurityConfigurerAdapter
                 "/v2/api-docs",
                 "/webjars/**")
             .permitAll()
-            .antMatchers(HttpMethod.POST,
-                "/users/**")
-            .hasAnyRole("ADMIN")
-            .antMatchers(HttpMethod.DELETE,
-                "/users/**")
-            .hasAnyRole("ADMIN")
-            .antMatchers(HttpMethod.PUT,
-                "/users/**")
-            .hasAnyRole("ADMIN")
 
             // *** NOTE AUTHENTICATED CAN READ USERS!!! PATCHES are handled in UserService
             .antMatchers("/users/**")
