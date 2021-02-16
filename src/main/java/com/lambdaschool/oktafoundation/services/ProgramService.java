cb62b827b4cab1bb89a0df42515e53a603baf293
bhawnishk1 <mrbhawnish@gmail.com>
2021-02-16 15:51:00 -0800

    Initial commit

---
+++ src/main/java/com/lambdaschool/oktafoundation/services/ProgramService.java
@@ -0,0 +1,20 @@
+package com.lambdaschool.oktafoundation.services;
+
+import com.lambdaschool.oktafoundation.models.Program;
+
+import java.util.List;
+
+public interface ProgramService
+{
+    Program save(Program program);
+
+    List<Program> findAll();
+
+    Program findProgramsById(long id);
+
+    Program findProgramsByName(String name);
+
+    void delete(long id);
+
+    Program update(Program program, long id);
+}
