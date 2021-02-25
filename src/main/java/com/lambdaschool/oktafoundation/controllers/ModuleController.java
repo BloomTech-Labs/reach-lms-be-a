3b438e0ea1a3e8ac3c02cf2639f3fd0f438cffe8
david chang <bjchang@Davids-MacBook-Air.local>
2021-02-24 19:00:33 -0800

    Added Module Controller

---
+++ src/main/java/com/lambdaschool/oktafoundation/controllers/ModuleController.java
@@ -0,0 +1,103 @@
+package com.lambdaschool.oktafoundation.controllers;
+
+import com.lambdaschool.oktafoundation.exceptions.ResourceNotFoundException;
+import com.lambdaschool.oktafoundation.models.Module;
+import com.lambdaschool.oktafoundation.repository.ModuleRepository;
+import com.lambdaschool.oktafoundation.services.ModuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ModuleController
+{
+    @Autowired
+    ModuleRepository modulerepos;
+
+    @Autowired
+    ModuleService moduleService;
+
+    @GetMapping(value = "/modules", produces = "application/json")
+    public ResponseEntity<?> getAllModules()
+    {
+        List<Module> modules = moduleService.findAll();
+
+        return new ResponseEntity<>(modules, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/modules/module/{moduleId}", produces = "application/json")
+    public ResponseEntity<?> getModuleById(@PathVariable Long moduleId)
+    {
+        Module module = moduleService.findModulesById(moduleId);
+
+        return new ResponseEntity<>(module, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/modules/module/{moduleName]", produces = "application/json")
+    public ResponseEntity<?> getModuleByName(@PathVariable String moduleName)
+    {
+        Module m = moduleService.findModulesByName(moduleName);
+        return new ResponseEntity<>(m, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/modules/{courseId}", produces = "application/json")
+    public ResponseEntity<?> getModulesByCourseId(@PathVariable Long courseId)
+    {
+        List<Module> allModules = new ArrayList<>();
+        modulerepos.findModulesByCourse_Courseid(courseId).iterator().forEachRemaining(allModules::add);
+
+        return new ResponseEntity<>(allModules, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/modules/{courseId}/module", produces = "application/json")
+    public ResponseEntity<?> addNewModule(@PathVariable long courseId, @Valid @RequestBody Module newModule) throws URISyntaxException
+    {
+        newModule.setModuleid(0);
+        newModule = moduleService.save(courseId, newModule);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newModuleURI = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{moduleid}")
+                .buildAndExpand(newModule.getModuleid())
+                .toUri();
+        responseHeaders.setLocation(newModuleURI);
+
+        return new ResponseEntity<>(newModule, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/modules/{moduleId}", consumes = "application/json")
+    public ResponseEntity<?> updateModule(@PathVariable long moduleId, @RequestBody Module newModule)
+    {
+        newModule.setModuleid(moduleId);
+        moduleService.update(moduleId, newModule);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/modules/{moduleId}", consumes = "application/json")
+    public ResponseEntity<?> updateFullModule(@PathVariable long moduleId, @RequestBody Module newModule)
+    {
+        newModule.setModuleid(moduleId);
+        moduleService.update(moduleId, newModule);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/modules/{moduleId}")
+    public ResponseEntity<?> deleteModuleById(@PathVariable long moduleId)
+    {
+        modulerepos.findById(moduleId).orElseThrow(() -> new ResourceNotFoundException("Module with id" + moduleId + " not found."));
+        moduleService.delete(moduleId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
