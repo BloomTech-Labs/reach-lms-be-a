04ff013dadd5390ef1e2d33311d5444c21ba7250
joel vega <joelvega97@yahoo.com>
2021-03-01 16:29:57 -0500

    made the building blocks for the serviceimpl tests.

---
+++ src/test/java/com/lambdaschool/oktafoundation/services/ProgramServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.lambdaschool.oktafoundation.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProgramServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findProgramsById() {
+    }
+
+    @Test
+    public void findProgramsByName() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
