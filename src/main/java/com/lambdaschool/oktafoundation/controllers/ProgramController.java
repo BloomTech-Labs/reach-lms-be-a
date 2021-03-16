789db075eaef908c75cc2e548422990025a1d7dc
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-03-16 12:32:44 -0400

    finish basic Program Links for Model & Controller

--- src/main/java/com/lambdaschool/oktafoundation/controllers/ProgramController.java
+++ src/main/java/com/lambdaschool/oktafoundation/controllers/ProgramController.java
@@ -1,14 +1,17 @@
 package com.lambdaschool.oktafoundation.controllers;
 
+
 import com.lambdaschool.oktafoundation.exceptions.ResourceNotFoundException;
+import com.lambdaschool.oktafoundation.modelAssemblers.ProgramModelAssembler;
 import com.lambdaschool.oktafoundation.models.Program;
 import com.lambdaschool.oktafoundation.repository.ProgramRepository;
 import com.lambdaschool.oktafoundation.services.ProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -16,88 +19,138 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
-public class ProgramController
-{
-    @Autowired
-    private ProgramService programService;
+public class ProgramController {
 
-    @Autowired
-    private ProgramRepository programRepos;
+	@Autowired
+	private ProgramService programService;
 
-    @GetMapping(value = "/programs", produces = "application/json")
-    public ResponseEntity<?> listAllPrograms()
-    {
-        List<Program> myPrograms = programService.findAll();
+	@Autowired
+	private ProgramRepository programRepos;
 
-        return new ResponseEntity<>(myPrograms, HttpStatus.OK);
-    }
+	@Autowired
+	private ProgramModelAssembler programModelAssembler;
 
-    @GetMapping(value = "/programs/program/{programId}", produces = "application/json")
-    public ResponseEntity<?> getProgramById(@PathVariable Long programId)
-    {
-        Program program = programService.findProgramsById(programId);
+	@GetMapping(value = "/programs", produces = "application/json")
+	public ResponseEntity<CollectionModel<EntityModel<Program>>> listAllPrograms() {
+		List<EntityModel<Program>> programEntities = programService.findAll()
+				.stream()
+				.map(programModelAssembler::toModel)
+				.collect(Collectors.toList());
 
-        return new ResponseEntity<>(program, HttpStatus.OK);
-    }
-    
-    @GetMapping(value = "/programs/program/name/{programName}", produces = "application/json")
-    public ResponseEntity<?> getProgramByName(@PathVariable String programName)
-    {
-        Program p = programService.findProgramsByName(programName);
-        return new ResponseEntity<>(p, HttpStatus.OK);
-    }
+		CollectionModel<EntityModel<Program>> collectionModel = CollectionModel.of(programEntities,
+				// Link to SELF -- `listAllPrograms` method
+				linkTo(methodOn(ProgramController.class).listAllPrograms()).withSelfRel()
+		);
+
+		return new ResponseEntity<>(collectionModel, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/programs/program/{programId}", produces = "application/json")
+	public ResponseEntity<EntityModel<Program>> getProgramById(
+			@PathVariable
+					Long programId
+	) {
+		EntityModel<Program> entityProgram = programModelAssembler.toModel(programService.findProgramsById(programId));
+		return new ResponseEntity<>(entityProgram, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/programs/program/name/{programName}", produces = "application/json")
+	public ResponseEntity<EntityModel<Program>> getProgramByName(
+			@PathVariable
+					String programName
+	) {
+		EntityModel<Program> entityProgram = programModelAssembler.toModel(programService.findProgramsByName(programName));
+		return new ResponseEntity<>(entityProgram, HttpStatus.OK);
+	}
 
 
+	@GetMapping(value = "/programs/{userid}", produces = "application/json")
+	public ResponseEntity<CollectionModel<EntityModel<Program>>> getProgramsByUserId(
+			@PathVariable
+					long userid
+	) {
+		List<EntityModel<Program>> programEntities = programRepos.findProgramsByUserid(userid)
+				.stream()
+				.map(programModelAssembler::toModel)
+				.collect(Collectors.toList());
 
-    @GetMapping(value = "/programs/{userid}", produces = "application/json")
-    public ResponseEntity<?> getProgramsByUserId(@PathVariable long userid)
-    {
-        List<Program> pList = programRepos.findProgramsByUserid(userid);
+		CollectionModel<EntityModel<Program>> collectionModel = CollectionModel.of(programEntities,
+				linkTo(methodOn(ProgramController.class).getProgramsByUserId(userid)).withSelfRel()
+		);
 
-        return new ResponseEntity<>(pList, HttpStatus.OK);
-    }
+		return new ResponseEntity<>(collectionModel, HttpStatus.OK);
+	}
 
-    @PostMapping(value = "/programs/{userid}/program", consumes = {"application/json"}, produces = "application/json")
-    public ResponseEntity<?> addNewProgram(@PathVariable long userid, @Valid @RequestBody Program newProgram) throws URISyntaxException
-    {
-        newProgram.setProgramid(0);
-        newProgram = programService.save(userid, newProgram);
+	@PostMapping(value = "/programs/{userid}/program", consumes = {"application/json"}, produces = "application/json")
+	public ResponseEntity<?> addNewProgram(
+			@PathVariable
+					long userid,
+			@Valid
+			@RequestBody
+					Program newProgram
+	)
+	throws URISyntaxException {
+		// TODO: check calling user's role permissions before we allow a POST
+		newProgram.setProgramid(0);
+		newProgram = programService.save(userid, newProgram);
 
-        HttpHeaders responseHeaders = new HttpHeaders();
-        URI newProgramURI = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{programid}")
-                .buildAndExpand(newProgram.getProgramid())
-                .toUri();
-        responseHeaders.setLocation(newProgramURI);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		URI newProgramURI = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{programid}")
+				.buildAndExpand(newProgram.getProgramid())
+				.toUri();
+		responseHeaders.setLocation(newProgramURI);
+		// TODO: send `newProgramURI` as part of the body
 
-        return new ResponseEntity<>(newProgram, responseHeaders, HttpStatus.CREATED);
-    }
+		return new ResponseEntity<>(newProgram, responseHeaders, HttpStatus.CREATED);
+	}
 
-    @PutMapping(value = "/programs/program/{programid}", consumes = "application/json")
-    public ResponseEntity<?> editEntireProgram(@Valid @RequestBody Program editProgram, @PathVariable Long programid)
-    {
-        editProgram.setProgramid(programid);
-        Program newtProgram = programService.update(editProgram, programid);
+	@PutMapping(value = "/programs/program/{programid}", consumes = "application/json")
+	public ResponseEntity<?> editEntireProgram(
+			@Valid
+			@RequestBody
+					Program editProgram,
+			@PathVariable
+					Long programid
+	) {
+		// TODO: check calling user's role permissions before we allow a PUT
+		editProgram.setProgramid(programid);
+		Program newtProgram = programService.update(editProgram, programid);
 
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
-    @PatchMapping(value = "/programs/program/{programid}", consumes = "application/json")
-    public ResponseEntity<?> editPartialProgram(@RequestBody Program editPartialProgram, @PathVariable Long programid)
-    {
-        programService.update(editPartialProgram, programid);
+	@PatchMapping(value = "/programs/program/{programid}", consumes = "application/json")
+	public ResponseEntity<?> editPartialProgram(
+			@RequestBody
+					Program editPartialProgram,
+			@PathVariable
+					Long programid
+	) {
+		// TODO: check calling user's role permissions before we allow a PATCH
+		programService.update(editPartialProgram, programid);
 
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
-    @DeleteMapping(value = "/programs/program/{programid}")
-    public ResponseEntity<?> deleteProgram(@PathVariable long programid)
-    {
-        programRepos.findById(programid).orElseThrow(() -> new ResourceNotFoundException("Program id with id" + programid + " Not found!"));
-        programRepos.deleteById(programid);
+	@DeleteMapping(value = "/programs/program/{programid}")
+	public ResponseEntity<?> deleteProgram(
+			@PathVariable
+					long programid
+	) {
+		// TODO: check calling user's role permissions before we allow a DELETE
+		programRepos.findById(programid)
+				.orElseThrow(() -> new ResourceNotFoundException("Program id with id" + programid + " Not found!"));
+		programRepos.deleteById(programid);
 
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
 }
--- src/main/java/com/lambdaschool/oktafoundation/modelAssemblers/ProgramModelAssembler.java
+++ src/main/java/com/lambdaschool/oktafoundation/modelAssemblers/ProgramModelAssembler.java
@@ -24,7 +24,10 @@ public class ProgramModelAssembler
 				linkTo(methodOn(ProgramController.class).getProgramByName(program.getProgramname())).withSelfRel(),
 				// Link to all programs by this program's admin
 				linkTo(methodOn(ProgramController.class).getProgramsByUserId(program.getUser()
-						.getUserid())).withRel("admin_programs")
+						.getUserid())).withRel("admin_programs"),
+				// Link to [GET all programs]
+				linkTo(methodOn(ProgramController.class).listAllPrograms()).withRel("all_programs")
+
 		);
 	}
 
