c9fe4c90b0b73bafcb4c7806f6d34389e573ccdd
david chang <bjchang@Davids-MacBook-Air.local>
2021-03-16 19:26:40 -0700

    Created model for UserCoursesId

--- src/main/java/com/lambdaschool/oktafoundation/models/UserCoursesId.java
+++ src/main/java/com/lambdaschool/oktafoundation/models/UserCoursesId.java
@@ -1,2 +1,54 @@
-package com.lambdaschool.oktafoundation.models;public class UserCoursesId {
+package com.lambdaschool.oktafoundation.models;
+
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class UserCoursesId implements Serializable
+{
+    private long user;
+    private long course;
+
+    public UserCoursesId()
+    {
+    }
+
+    public long getUser()
+    {
+        return user;
+    }
+
+    public void setUser(long user)
+    {
+        this.user = user;
+    }
+
+    public long getCourse()
+    {
+        return course;
+    }
+
+    public void setCourse(long course)
+    {
+        this.course = course;
+    }
+
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        UserCoursesId that = (UserCoursesId) o;
+        return user == that.user &&
+                course == that.course;
+    }
+
+    @Override
+    public int hashCode(){return 37;}
 }
