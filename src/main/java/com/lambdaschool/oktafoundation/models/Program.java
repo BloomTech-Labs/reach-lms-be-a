96888414d7715428430b96cd0ebd1a4b453801f4
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-03-21 22:05:37 -0400

    fucking around with more Okta goodness

--- src/main/java/com/lambdaschool/oktafoundation/models/Program.java
+++ src/main/java/com/lambdaschool/oktafoundation/models/Program.java
@@ -1,117 +1,96 @@
 package com.lambdaschool.oktafoundation.models;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
 
 @Entity
 @Table(name = "programs")
-@JsonIgnoreProperties(value={"courses", "user"})
-public class Program extends Auditable
-{
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long programid;
+@JsonIgnoreProperties(value = {"courses", "user"})
+public class Program
+		extends Auditable {
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "program", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnoreProperties(value = "program", allowSetters = true)
+	List<Course> courses = new ArrayList<>();
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long   programid;
+	private String programname;
+	private String programtype;
+	private String programdescription;
+	@ManyToOne
+	@JoinColumn(name = "userid", nullable = false)
+	@JsonIgnoreProperties(value = "programs")
+	private User user;
 
-    private String programname;
+	public Program() {
+	}
 
+	public Program(
+			String programname,
+			String programtype,
+			String programdescription
+	) {
+		this.programname        = programname;
+		this.programtype        = programtype;
+		this.programdescription = programdescription;
+	}
 
-    private String programtype;
+	public long getProgramid() {
+		return programid;
+	}
 
-    private String programdescription;
+	public void setProgramid(long programid) {
+		this.programid = programid;
+	}
 
-    @OneToMany(fetch = FetchType.LAZY,
-    mappedBy = "program",
-    cascade = CascadeType.ALL,
-    orphanRemoval = true)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JsonIgnoreProperties(value = "program",
-        allowSetters = true)
-    List<Course> courses = new ArrayList<>();
+	public String getProgramname() {
+		return programname;
+	}
 
-    @ManyToOne
-    @JoinColumn(name = "userid", nullable = false)
-    @JsonIgnoreProperties(value = "programs")
-    private User user;
+	public void setProgramname(String programname) {
+		this.programname = programname;
+	}
 
+	public String getProgramtype() {
+		return programtype;
+	}
 
-    public Program()
-    {
-    }
+	public void setProgramtype(String programtype) {
+		this.programtype = programtype;
+	}
 
-    public Program(String programname, String programtype, String programdescription)
-    {
-        this.programname = programname;
-        this.programtype = programtype;
-        this.programdescription = programdescription;
-    }
+	public String getProgramdescription() {
+		return programdescription;
+	}
 
-    public long getProgramid()
-    {
-        return programid;
-    }
+	public void setProgramdescription(String programdescription) {
+		this.programdescription = programdescription;
+	}
 
-    public void setProgramid(long programid)
-    {
-        this.programid = programid;
-    }
+	public User getUser() {
+		return user;
+	}
 
-    public String getProgramname()
-    {
-        return programname;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    public void setProgramname(String programname)
-    {
-        this.programname = programname;
-    }
+	public List<Course> getCourses() {
+		return courses;
+	}
 
-    public String getProgramtype()
-    {
-        return programtype;
-    }
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
 
-    public void setProgramtype(String programtype)
-    {
-        this.programtype = programtype;
-    }
-
-    public String getProgramdescription()
-    {
-        return programdescription;
-    }
-
-    public void setProgramdescription(String programdescription)
-    {
-        this.programdescription = programdescription;
-    }
-
-    public User getUser()
-    {
-        return user;
-    }
-
-    public void setUser(User user)
-    {
-        this.user = user;
-    }
-
-    public List<Course> getCourses()
-    {
-        return courses;
-    }
-
-    public void setCourses(List<Course> courses)
-    {
-        this.courses = courses;
-    }
 }
 
