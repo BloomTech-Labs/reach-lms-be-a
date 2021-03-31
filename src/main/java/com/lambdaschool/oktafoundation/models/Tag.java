95a164beb5057bac9bc0476c8e13f085266b68ca
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-03-30 21:33:22 -0400

    remove @NaturalId annotation and Caching for Tags to allow changing of Tag.title

--- src/main/java/com/lambdaschool/oktafoundation/models/Tag.java
+++ src/main/java/com/lambdaschool/oktafoundation/models/Tag.java
@@ -3,9 +3,6 @@ package com.lambdaschool.oktafoundation.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.hibernate.annotations.NaturalId;
-import org.hibernate.annotations.NaturalIdCache;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -16,33 +13,30 @@ import java.util.Set;
 
 @Entity
 @Table(name = "tags")
-@NaturalIdCache
-@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @JsonIgnoreProperties(value = "courses")
 public class Tag
 		extends Auditable {
 
-	private static final String DEFAULT_HEX_CODE = "#FFFFFF";
+	private static final String           DEFAULT_HEX_CODE = "#FFFFFF";
 	//
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long             tagid;
+	private              long             tagid;
 	//
 	@NotNull
-	@NaturalId
-	private String           title;
+	private              String           title;
 	//
 	@NotNull
-	private String           hexcode;
+	private              String           hexcode;
 	//
 	@OneToMany(mappedBy = "tag", cascade = CascadeType.ALL, orphanRemoval = true)
 	@JsonIgnore
-	private Set<ProgramTags> programs = new HashSet<>();
+	private              Set<ProgramTags> programs         = new HashSet<>();
 	//
 	@ManyToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name = "courseid")
 	@JsonIgnoreProperties(value = "tags")
-	private Set<ProgramTags> courses  = new HashSet<>();
+	private              Set<ProgramTags> courses          = new HashSet<>();
 
 	public Tag() {}
 
